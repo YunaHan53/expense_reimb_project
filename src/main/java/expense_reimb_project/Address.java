30e04fab7be053b89442deb03e71af2918b16105
YunaH <YunaH503@gmail.com>
2021-06-27 16:15:15 -0400

    Added source folders

---
+++ src/main/java/expense_reimb_project/Address.java
@@ -0,0 +1,34 @@
+package expense_reimb_project;
+
+public class Address {
+
+	private String city;
+	private String state;
+	
+	public Address(String city, String state) {
+		this.city = city;
+		this.state = state;
+	}
+	
+	// Getters/Setters
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return city + ", " + state;
+	}
+}
