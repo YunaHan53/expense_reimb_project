20583fcfd4131b41aae6ba35e2559e5ae8b5c8ec
YunaH <YunaH503@gmail.com>
2021-07-06 18:38:10 -0400

    created login and register pages, got pageswitching working

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,25 +1,19 @@
-package Main;
+package servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-public class Login extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID =1L;
 
-	public Login() {
-		super();
-	}
-
+	@Override
 	protected void doGet(HttpServletRequest request,HttpServletResponse response) 
 			throws ServletException, IOException{
-		response.sendRedirect("/login.html");
-
-
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/login.html").forward(request, response);
 	}
 }
 
