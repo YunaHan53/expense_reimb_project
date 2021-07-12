4ea14b8e2e22406c6d01fe5c86456d640106ab81
YunaH <YunaH503@gmail.com>
2021-07-12 02:55:10 -0400

    Retrieved all employees to print on the manager page

--- src/main/java/services/ManagerServiceImp.java
+++ src/main/java/services/ManagerServiceImp.java
@@ -1,9 +1,11 @@
 package services;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import con.ConnectionUtil;
 import models.Manager;
@@ -15,25 +17,32 @@ public class ManagerServiceImp implements ManagerServices {
 	// IMPLEMENTATION //
 	
 	// View all Employees
-	public void viewEmployees() {
+	public List<User> viewAllEmployees() {
 		System.out.println("Viewing all employees\n");
 		
-		String QUERY = "select userId, firstName, lastName, userName, email from UserRegister";
+		List<User> u = new ArrayList<User>();
+		String QUERY = "select * from userregister order by id asc";
+					
 		try(Connection con = ConnectionUtil.getConnection();
-				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(QUERY))	
+				PreparedStatement ps = con.prepareStatement(QUERY);
+				ResultSet rs = ps.executeQuery())	
 		    {	
-				while(rs.next()){
-				int userId = rs.getInt("userId");
+			while (rs.next()) {
+				int userId = rs.getInt("id");
 				String firstName = rs.getString("firstName");
 				String lastName = rs.getString("lastName");
 				String userName = rs.getString("userName");
+				String pass = rs.getString("pass");
 				String email = rs.getString("email");
-				System.out.println(userId + " | " + firstName + " " + lastName  + " | " + userName + " | " + email + " \n");} 
+				String jobPosition = rs.getString("jobPosition");
+				u.add(new User(userId, firstName, lastName, userName, email, pass, jobPosition));
+							
+				} 
 		    } catch (SQLException e)
 			{
 		    	e.printStackTrace(); 
 		    }
+		return u;
 	}
 	// Approve Requests (Manager m, Request r)
 	public void approveRequest(Manager m, Request r) {
--- src/main/java/services/ManagerServices.java
+++ src/main/java/services/ManagerServices.java
@@ -1,12 +1,15 @@
 package services;
 
+import java.util.List;
+
 import models.Manager;
 import models.Request;
+import models.User;
 
 public interface ManagerServices {
 
 	// Declaration
-	public void viewEmployees();
+	public List<User> viewAllEmployees();
 	public void approveRequest(Manager m, Request r);
 	public void denyRequest(Manager m, Request r);
 	public void restorePassword(String userName, String email);
