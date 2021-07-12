4100b843c179e9d17789528243a71ecb9e3a7d11
YunaH <YunaH503@gmail.com>
2021-07-12 02:57:27 -0400

    Modified Junit test file from Test to Tests, fixed errors

--- src/test/java/ers/Tests.java
+++ src/test/java/ers/Tests.java
@@ -1,17 +1,31 @@
 package ers;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
 
 import java.sql.Connection;
 
 import con.ConnectionUtil;
+import services.UserServicesImp;
 
-public class Test {
+public class Tests {
+	private UserServicesImp usi;
 	
+	@Test
     public void testConnection() {
     	Connection con = ConnectionUtil.getConnection();
     	assertNotNull(con);
     }
     
+	@Test
+    void login() {
+    	assertEquals("MrSponge", usi.login("MrSponge", "krusty", "manager").getFirstName());
+    }
+    
+	@Test
+	void register() {
+		assertEquals(false,usi.register(-1, null, null, null, null, null, null, null));
+	}
     
 }
